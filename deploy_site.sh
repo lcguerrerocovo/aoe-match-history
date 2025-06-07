6b52206148d071a57cd3c4662fdbc8fc28c27a61
Luis Guerrero <lcguerrerocovo@gmail.com>
2025-06-07 01:37:05 -0400

    scoped acl settings per deployment

--- deploy_site.sh
+++ deploy_site.sh
@@ -15,20 +15,14 @@ mkdir -p site/matches
 echo "[deploy] Uploading match data"
 gsutil -m cp -n -r data/matches/* gs://aoe2.site/data/matches/
 gsutil -m cp -n -r site/matches/* gs://aoe2.site/site/matches/ || true
-gsutil -m cp data/100.json gs://aoe2.site/data/100.json
 
 # Force no-cache on index.json
 echo "[deploy] Forcing no-cache on index.json"
 gsutil -h "Cache-Control:no-cache, max-age=0" \
   cp data/matches/index.json gs://aoe2.site/data/matches/index.json
 
-# Upload civ map with long cache
-echo "[deploy] Uploading civ map with long cache"
-gsutil -h "Cache-Control:public, max-age=86400" cp data/100.json \
-  gs://aoe2.site/data/100.json
-
-# Make everything public
-echo "[deploy] Setting public ACL on gs://aoe2.site"
-gsutil -m acl ch -r -u AllUsers:R gs://aoe2.site/
+# Make match data public
+echo "[deploy] Setting public ACL on match data"
+gsutil -m acl ch -r -u AllUsers:R gs://aoe2.site/index.htmlgs://aoe2.site/data/matches/ gs://aoe2.site/site/matches/
 
 echo "[deploy] Completed."
